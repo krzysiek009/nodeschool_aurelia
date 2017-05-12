ca89a56af20aba77472dde53a5f6c99e02c0c804
krzysiek009 <krzysiek009@poczta.fm>
2017-05-13 00:48:01 +0200

    Step 1

---
+++ src/app.js
@@ -0,0 +1,22 @@
+//import {computedFrom} from 'aurelia-framework';
+
+export class App {
+  heading = 'Welcome to the Aurelia Navigation App!';
+  firstName = 'John';
+  lastName = 'Doe';
+  previousValue = this.fullName;
+
+  //Getters can't be directly observed, so they must be dirty checked.
+  //However, if you tell Aurelia the dependencies, it no longer needs to dirty check the property.
+  //To optimize by declaring the properties that this getter is computed from, uncomment the line below
+  //as well as the corresponding import above.
+  //@computedFrom('firstName', 'lastName')
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  submit() {
+    this.previousValue = this.fullName;
+    alert(`Welcome, ${this.fullName}!`);
+  }
+}
