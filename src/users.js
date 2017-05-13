6fe4a0339fb78fece8d3bfe8b727d8193ed2ba91
krzysiek009 <krzysiek009@poczta.fm>
2017-05-13 02:45:54 +0200

    Prepare for the Fetch

--- src/users.js
+++ src/users.js
@@ -1,3 +1,24 @@
+import { inject, Lazy } from 'aurelia-framework';
+import { HttpClient } from 'aurelia-fetch-client';
+
+// polyfill fetch client conditionally
+const fetch = !self.fetch ? System.import('isomorphic-fetch') : Promise.resolve(self.fetch);
+
+@inject(Lazy.of(HttpClient))
 export class Users {
   heading = 'Github Users';
+  users = [];
+
+  constructor(getHttpClient) {
+    this.getHttpClient = getHttpClient;
+  }
+
+  async activate() {
+    // ensure fetch is polyfilled before we create the http client
+    await fetch;
+    const http = this.http = this.getHttpClient();
+
+    // Fetch the JSON data from https://api.github.com/users
+
+  }
 }
